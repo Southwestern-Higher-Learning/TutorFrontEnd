32ca349332208f2ee99ac693160728a4faef126f
Daniel Whitney <whitneyd@southwestern.edu>
2021-01-31 12:40:57 -0600

    removed header at change to bottom tab navi

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {LoginScreen} from './src/screens/LoginScreen';
 import {HonorCodeScreen} from './src/screens/HonorCodeScreen';
 import {HomeTabs} from './src/navigators/HomeTabs';
@@ -9,7 +8,6 @@ import {HomeTabs} from './src/navigators/HomeTabs';
 
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
 
 function App() {
   return (
@@ -17,7 +15,7 @@ function App() {
       <Stack.Navigator>
         <Stack.Screen name="LoginScreen" component={LoginScreen} options={{title: "Log in"}}/>
         <Stack.Screen name="HonorCodeScreen" component={HonorCodeScreen} options={{title: "Honor Code"}}/>
-        <Stack.Screen name="HomeTabs" component={HomeTabs} />
+        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
