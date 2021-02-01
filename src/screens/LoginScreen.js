4ffb6bf698f513efd15be0705408943db35cd48c
Jake Gutierrez <jakegut0108@gmail.com>
2021-02-01 13:07:57 -0600

    Add way to get code, need to get access code

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native'
+import * as AuthSession from 'expo-auth-session';
 
 
 export const LoginScreen = ({navigation})=>{
+
+    _asyncSignIn = async () => {
+        const googleWebAppId = "217927433662-hkbspdt6kb2vm5p3eoolug35d1jp1obh.apps.googleusercontent.com";
+        const redirectUrl = AuthSession.makeRedirectUri({useProxy: true});
+        let result = await AuthSession.startAsync({
+            authUrl:
+              `https://accounts.google.com/o/oauth2/v2/auth?` +
+              `&client_id=${googleWebAppId}` +
+              `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+              `&response_type=code` +
+              `&access_type=offline` +
+              `&scope=profile`,
+          });
+        console.log(result);
+    }
+
     return (
         <View style={styles.container}>
             <Text>Login Screen</Text>
             <Button
-                onPress={()=>{
-                    navigation.navigate('HonorCodeScreen')
-                }}
+                onPress={this._asyncSignIn}
                 title="Log in"
                 accessibilityLabel="Learn more about this purple button"
             />
