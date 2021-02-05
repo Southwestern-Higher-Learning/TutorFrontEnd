ae8e3ac10f94f739cd057959ebc10bcaa882a1bd
Daniel Whitney <whitneyd@southwestern.edu>
2021-02-04 19:55:09 -0600

    back to base set up

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native'
-import * as AuthSession from 'expo-auth-session';
 
 
 export const LoginScreen = ({navigation})=>{
 
-    _asyncSignIn = async () => {
-        const googleWebAppId = "217927433662-hkbspdt6kb2vm5p3eoolug35d1jp1obh.apps.googleusercontent.com";
-        const redirectUrl = AuthSession.makeRedirectUri({useProxy: true});
-        let result = await AuthSession.startAsync({
-            authUrl:
-              `https://accounts.google.com/o/oauth2/v2/auth?` +
-              `&client_id=${googleWebAppId}` +
-              `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
-              `&response_type=code` +
-              `&access_type=offline` +
-              `&scope=profile` + 
-              `&prompt=consent`,
-          });
-        console.log(result);
-    }
+
 
     return (
         <View style={styles.container}>
             <Text>Login Screen</Text>
             <Button
-                onPress={this._asyncSignIn}
+                onPress={()=>navigation.navigate('HonorCodeScreen')}
                 title="Log in"
                 accessibilityLabel="Learn more about this purple button"
             />
