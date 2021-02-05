221a0a3dfb399d9cf161caa7088d5e1154392a7e
Daniel Whitney <whitneyd@southwestern.edu>
2021-02-04 19:59:39 -0600

    updated loginscreen background color

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -22,7 +22,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: "#f3f3f3"
 
     }
 })
