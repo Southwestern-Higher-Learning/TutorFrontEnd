5c5a629c2cb4638f9d36613c4be3701a7cc25a66
Jake Gutierrez <jakegut0108@gmail.com>
2021-02-06 15:18:02 -0600

    finally

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -5,27 +5,42 @@ import * as AuthSession from 'expo-auth-session';
 
 export const LoginScreen = ({navigation})=>{
 
-    _asyncSignIn = async () => {
-        const googleWebAppId = "217927433662-hkbspdt6kb2vm5p3eoolug35d1jp1obh.apps.googleusercontent.com";
+    const signIn = async () => {
+        console.log("weee")
+        const infoResponse = await fetch('https://tutor.jakegut.com/auth/code/url')
+        const info = await infoResponse.json()
         const redirectUrl = AuthSession.makeRedirectUri({useProxy: true});
         let result = await AuthSession.startAsync({
             authUrl:
-              `https://accounts.google.com/o/oauth2/v2/auth?` +
-              `&client_id=${googleWebAppId}` +
+              `https://accounts.google.com/o/oauth2/auth?` +
+              `&client_id=${info['client_id']}` +
               `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
               `&response_type=code` +
               `&access_type=offline` +
-              `&scope=profile` + 
+              `&scope=${encodeURIComponent(info['scopes'].join(' '))}` + 
               `&prompt=consent`,
           });
         console.log(result);
+        if (result['errorCode'] == null) {
+            console.log("Getting accesstoken...")
+            fetch("https://tutor.jakegut.com/auth/swap", {
+                method: 'POST',
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({"code": result['params']['code'], "redirect_uri": redirectUrl})
+            })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+        }
     }
 
     return (
         <View style={styles.container}>
             <Text>Login Screen</Text>
             <Button
-                onPress={this._asyncSignIn}
+                onPress={signIn}
                 title="Log in"
                 accessibilityLabel="Learn more about this purple button"
             />
