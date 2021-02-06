5ff4075cefc82252765f5f6ef938059e01a458ff
EthanNunez <70860088+EthanNunez@users.noreply.github.com>
2021-02-06 11:31:44 -0600

    Updated the Login screen styling.

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import {LoginScreen} from './src/screens/LoginScreen';
 import {HonorCodeScreen} from './src/screens/HonorCodeScreen';
 import {HomeTabs} from './src/navigators/HomeTabs';
+import { setStatusBarBackgroundColor } from 'expo-status-bar';
 
 
 
@@ -11,14 +12,13 @@ const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{title: "Log in"}}/>
-        <Stack.Screen name="HonorCodeScreen" component={HonorCodeScreen} options={{title: "Honor Code"}}/>
-        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="LoginScreen" component={LoginScreen} options={{title: "Southwestern Higher Learning", headerStyle:{backgroundColor: '#ffcd20'}}}/>
+          <Stack.Screen name="HonorCodeScreen" component={HonorCodeScreen} options={{title: "Honor Code"}}/>
+          <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
   );
-}
-
+} 
 export default App;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native'
+import {View, Text, StyleSheet, Pressable, ImageBackground} from 'react-native';
 
 
 export const LoginScreen = ({navigation})=>{
     return (
         <View style={styles.container}>
-            <Text>Login Screen</Text>
-            <Button
+            <ImageBackground
+                source={require('../../assets/LoginPage.jpg')}
+                style={styles.image}
+            />
+            <ImageBackground
+                source={require('../../assets/Gradient.jpg')}
+                style={styles.imageGradient}
+            />
+            <ImageBackground
+                source={require('../../assets/CrestNameLogoWhite.png')}
+                style={styles.logo}
+            />
+            <View style={styles.buttonContainer}>
+            <Pressable
+                style={styles.button}
                 onPress={()=>{
                     navigation.navigate('HonorCodeScreen')
                 }}
-                title="Log in"
-                accessibilityLabel="Learn more about this purple button"
-            />
+            >
+                <Text style={styles.buttonText}>Log in</Text>
+
+            </Pressable>
+            </View>
         </View>
     )
 }
@@ -20,8 +35,53 @@ export const LoginScreen = ({navigation})=>{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
         justifyContent: 'center',
-        alignItems: 'center'
+
+    },
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 90,
+        alignItems: 'center',
+        width: '100%',
+        padding: 10,
+    },
+    button: {
+        height: 45,
+        width: '80%',
+        opacity: 0.9,
+        borderRadius: 23, 
+        borderColor: 'black',
+        backgroundColor: '#ffcd20',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        fontSize: 30,
+        fontStyle: 'italic',
+        fontWeight: '500',
+        textTransform: 'uppercase',
+    },
+    image: {
+        width:'100%',
+        height: '100%',
+        resizeMode: 'cover',
+        position: 'absolute',
+    },
+    imageGradient: {
+        width:'100%',
+        height: '100%',
+        resizeMode: 'cover',
+        position: 'absolute',
+        opacity: 0.40,
+    },
+    logo: {
+        height: 275,
+        width: 375,
+        resizeMode: 'contain',
+        position: 'absolute',
+        top: 30,
 
     }
+
 })
