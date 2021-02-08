a91c740f01225af2cba148a1ed17ae802af16970
Daniel Whitney <whitneyd@southwestern.edu>
2021-02-07 20:18:06 -0600

    updating with wip - THIS DOES NOT WORK

--- src/navigators/HomeTabs.js
+++ src/navigators/HomeTabs.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { HomeScreen } from '../screens/HomeScreen'
 import { ProfileScreen } from '../screens/ProfileScreen'
 import { SearchScreen } from '../screens/SearchScreen'
-const Tab = createBottomTabNavigator()
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+const Tab = createMaterialBottomTabNavigator()
 
 export const HomeTabs = ()=>{
     return (
         <Tab.Navigator
+        initialRouteName="Home"
+        activeColor="#000000"
+        inactiveColor="#828282"
+        barStyle={{ backgroundColor: '#FFCD00'}}
+
         >
-            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Home" component={HomeScreen} 
+            options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({color})=>(<MaterialCommunityIcons name="home" color={color} size={26} />)
+            }}
+            />
             <Tab.Screen name="Search" component={SearchScreen} />
             <Tab.Screen name="Profile" component={ProfileScreen} />
         </Tab.Navigator>
