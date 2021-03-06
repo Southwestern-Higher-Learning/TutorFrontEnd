54f1fdf5587270188a6225d918f381531b2ce036
Whitney Whitehill <whitehiw@southwestern.edu>
2021-03-05 22:25:06 -0600

    Worked on switching from fetch to axios. DOES NOT WORK

--- src/providers/LoginUser.js
+++ src/providers/LoginUser.js
@@ -1,6 +1,57 @@
 import * as AuthSession from 'expo-auth-session'
+import axios from 'axios'
 
-export const LoginUser = async (navigation) => {
+export const LoginUser= async () => {
+    let info
+    const infoResponse = await axios.get('https://tutor.jakegut.com/auth/code/url')
+    .then((response)=>{
+        if (response.status === 200){
+            info = response.data
+            const result = await startAsync()
+              if (result['errorCode'] === null) {
+                    axios.post('https://tutor.jakegut.com/auth/swap', {
+                    body: JSON.stringify({"code": result['params']['code'], "redirect_uri": redirectUrl}),
+                    Headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    
+                }).then((response)=>{
+                    if(response.status === 200){
+                        console.log(data)
+                        return response.data
+                    }
+                }, (error) => console.log(error))
+
+                }
+        } 
+    }).catch(err => console.log(err))
+    
+
+}
+
+
+const startAsync = async ()=>{
+    const redirectUrl = AuthSession.makeRedirectUri({useProxy: true});
+    let result = AuthSession.startAsync({
+        authUrl:
+            `https://accounts.google.com/o/oauth2/auth?` +
+            `&client_id=${info['client_id']}` +
+            `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+            `&response_type=code` +
+            `&access_type=offline` +
+            `&scope=${encodeURIComponent(info['scopes'].join(' '))}` + 
+            `&prompt=consent`,
+        });
+    return result
+}
+
+
+
+
+
+
+export const LoginU = async (navigation) => {
+
     const infoResponse = await fetch('https://tutor.jakegut.com/auth/code/url')
     const info = await infoResponse.json()
     const redirectUrl = AuthSession.makeRedirectUri({useProxy: true});
@@ -30,3 +81,4 @@ export const LoginUser = async (navigation) => {
     }
     return Promise.reject()
 }
+
