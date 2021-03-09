612a7f85f33be990c440169c92af9f26d0bf9c08
Whitney Whitehill <whitehiw@southwestern.edu>
2021-03-08 22:06:39 -0600

    Resolved issue 16, added global auth header

--- src/providers/LoginUser.js
+++ src/providers/LoginUser.js
@@ -1,10 +1,6 @@
 import * as AuthSession from 'expo-auth-session'
 import axios from 'axios'
 
-
-
-
-
 export const LoginUser = async () => {
     // initiate login process with exterior request
     const info = await getInitialRequest()
@@ -30,10 +26,7 @@ export const LoginUser = async () => {
                 }
             })
             .then(res => {
-                // at this point we have successfully obtained the user info
-                // now we want to set the global axios authorization header with the users access_token
-                // after we set the access_token, in the updateaboutme provider, we can remove the authorization token
-                // then refactor to remove the access_token from the function that calls it
+                axios.defaults.headers.common['Authorization'] =  `Bearer ${res.data['access_token']}`
                 return res.data})
             .catch(err =>  {return Promise.reject(err)})
             }
@@ -47,46 +40,8 @@ export const LoginUser = async () => {
 }
 
 
+
 const getInitialRequest = async ()=>{
     const req = await axios.get('https://tutor.jakegut.com/auth/code/url')
     return req.data
 }
-
-
-
-export const LoginU = async (navigation) => {
-
-    const infoResponse = await fetch('https://tutor.jakegut.com/auth/code/url')
-    const info = await infoResponse.json()
-    console.log(info)
-    const redirectUrl = AuthSession.makeRedirectUri({useProxy: true});
-    console.log(redirectUrl)
-    let result = await AuthSession.startAsync({
-        authUrl:
-          `https://accounts.google.com/o/oauth2/auth?` +
-          `&client_id=${info['client_id']}` +
-          `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
-          `&response_type=code` +
-          `&access_type=offline` +
-          `&scope=${encodeURIComponent(info['scopes'].join(' '))}` + 
-          `&prompt=consent`,
-      });
-
-    console.log(result) 
-    if (result['errorCode'] == null) {
-        return fetch("https://tutor.jakegut.com/auth/swap", {
-            method: 'POST',
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({"code": result['params']['code'], "redirect_uri": redirectUrl})
-        })
-        .then(response => response.json())
-        .then((data) => {
-            return data
-        })
-        .catch(err => {return err})
-    }
-    return Promise.reject()
-}
-
--- src/providers/UpdateAboutMe.js
+++ src/providers/UpdateAboutMe.js
@@ -3,21 +3,22 @@ import axios from 'axios'
 
 export const UpdateDescription = async (description, access_token)=>{
 
-    const response = await axios.patch(`https://tutor.jakegut.com/user/me`,
-    
-    {
-        description
-    },
-    {
-        headers: {
-            'Authorization': `Bearer ${access_token}`
+    try {
+        const response = await axios.patch(`https://tutor.jakegut.com/user/me`,
+        
+        {
+            description
         }
-    })
-    
-    if(response.status === 200){
-        return response.data
-    } else {
-        return null
+        )
+        
+        if(response.status === 200){
+            return response.data
+        } else {
+            return null
+        }
+    } catch (error) {
+        // Error handling TBD
+        console.log(error)
     }
 
 }
